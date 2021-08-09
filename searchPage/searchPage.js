0ee5a402f17bac8451d97e22419134ef341d07d2
adnan-afana <adnanafana0@gmail.com>
2021-08-09 22:42:54 +0300

    move the get started button function that go to recomendation page from searchPage.js to landingPage.js Relates #31

--- searchPage/searchPage.js
+++
@@ -1,7 +0,0 @@
-// Create function to move to search Page
-let startButton = document.querySelector(".start-button");
-startButton.addEventListener("click", moveToSearchPage);
-
-function moveToSearchPage() {
-  window.location.href = "searchPage/searchPage.html";
-}
