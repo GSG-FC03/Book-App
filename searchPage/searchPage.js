cff9efd32e08624f98f491dc2fcae70f19b95120
WessalJawad95 <wessaljawad95@gmail.com>
2021-08-10 12:17:37 +0300

    editing in searchPage.html and editing the styles in searchPage.css and editing in the searchPage.js to show the result of search process by using api

--- searchPage/searchPage.js
+++ searchPage/searchPage.js
@@ -1,15 +1,59 @@
+// data.items[0].volumeInfo.imageLinks.smallThumbnail
 const searchIcon = document.querySelector(".search-icon")
 const searchInput = document.querySelector(".search-input")
 const resultsArea = document.querySelector(".container")
-// console.log("5555",searchIcon,searchIcon,resultsArea)
+
+
+
+
 
 searchIcon.addEventListener("click",searchResult)
 function searchResult(){
     // console.log("969999",searchInput.value)  
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput.value}`)
     .then(res=>res.json())
-    .then(data => console.log(data))
+    .then(data =>{
+        if(searchInput.value===null||searchInput.value==="")
+        {
+            alert("write a key word")
+        }
+        else {
+    for(let i=0; i<=10;i++){
+    let BookTitle=data.items[i].volumeInfo.title;
+    let BookDesciption=data.items[i].volumeInfo.desciption;
+    let BookImg=data.items[i].volumeInfo.imageLinks.smallThumbnail;
+    
+     card=document.createElement("div")
+     card.classList.add("card")
+
+     cardImage=document.createElement("img")
+      cardImage.classList.add("card")
+     cardImage.innerHTML=BookImg;
+    
+     cardContent=document.createElement("div")
+     cardContent.classList.add("card-content")
+     
+
+     cardContentTitle=document.createElement("h4")
+     cardContentTitle.classList.add("card-content-title")
+     cardContentTitle.innerHTML=BookTitle;
+
+     cardContentDesciption=document.createElement("p")
+     cardContentDesciption.classList.add("card-content-description")
+     cardContentDesciption.innerHTML=BookDesciption;
+     
+     resultsArea.appendChild(card)
+     card.appendChild(cardImage)
+     resultsArea.appendChild(cardContent)
+     cardContent.appendChild(cardContentTitle)
+     cardContent.appendChild(cardContentDesciption)
+
+    }
+        }
+    })
     .catch(error => console.log("Error:",error))
 }
+
+
 
 
