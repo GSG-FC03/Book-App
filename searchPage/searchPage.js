d2a89eed8a83502fbb73c7fc8ce37be4bee4d5f8
adnan-afana <adnanafana0@gmail.com>
2021-08-12 17:56:14 +0300

    give the book card another class which is the book ididentifier to make it as an id to be able to call it in bookMusicPage

--- searchPage/searchPage.js
+++ searchPage/searchPage.js
@@ -1,16 +1,13 @@
-// data.items[0].volumeInfo.imageLinks.smallThumbnail
+// DOM
 const searchIcon = document.querySelector(".search-icon")
 const searchInput = document.querySelector(".search-input")
 const resultsArea = document.querySelector(".container")
 
-
-
-
-
-
+// Show the search result when click at search icon
 searchIcon.addEventListener("click",searchResult)
+
+// Fetching the API  
 function searchResult(){
-    // console.log("969999",searchInput.value)  
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput.value}`)
     .then(res=>res.json())
     .then(data =>{
@@ -25,10 +22,10 @@ function searchResult(){
     let BookTitle=data.items[i].volumeInfo.title; // fetch title from api
     let BookDesciption=data.items[i].volumeInfo.description; // fetch description from api
     let BookImg=data.items[i].volumeInfo.imageLinks.smallThumbnail; // fetch link of image from api
-    let id =data.items[i].volumeInfo.industryIdentifiers[0].identifier;
+    let id =data.items[i].volumeInfo.industryIdentifiers[0].identifier; //Book id
         
      card=document.createElement("div") 
-     card.classList.add("card", id)
+     card.classList.add("card", id) // Give the card two class one for style the second is the ID
      card.addEventListener("click",getIdToGetDetails)
 
      cardImage=document.createElement("div")
@@ -53,6 +50,7 @@ function searchResult(){
      cardContentDesciption.classList.add("card-content-description")
      cardContentDesciption.innerHTML=BookDesciption;
      
+    // make the class parents and chlid
      resultsArea.appendChild(card)
      card.appendChild(cardImage)
      cardImage.appendChild(imgElement)
@@ -65,10 +63,8 @@ function searchResult(){
     })
     .catch(error => console.log("Error:",error))
 }
-// card.addEventListener("click",open)
-// function open(){
 
-// }
+// Git the book ID and move result page (bookMusicPage)
 
 function getIdToGetDetails(ele){
 let cardClassList = [];
